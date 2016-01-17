068eb4ebc8fa1b84d37eb8d4d7f955d1ee94c977
Spencer Krum <nibz@spencerkrum.com>
2016-01-16 21:13:16 -0800

    Beta test for vpci

---
+++ test_collectd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+repo="https://github.com/voxpupuli/puppet-collectd"
+ref=""
+
+VPCI_ROOT=~/vcpi
+
+${VPCI_ROOT}/scripts/allow_local_ssh_root
+${VPCI_ROOT}/scripts/clone_repo $repo $ref
+cd puppet-collectd
+${VPCI_ROOT}/scripts/write_nodeset
+${VPCI_ROOT}/scripts/run_beaker_rspec
