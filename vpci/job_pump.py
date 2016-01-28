5cc55b6dca69cd549e492e16a02d0e5a688ed946
Spencer Krum <nibz@spencerkrum.com>
2016-01-28 10:29:00 -0800

    Job pumper can create jobs and push them out

--- vpci/job_pump.py
+++ vpci/job_pump.py
@@ -1,9 +1,43 @@
 #!/usr/bin/env python
 
 import time
+import pprint
 
+import json
 import redis
 import yaml
+
+job_format = {
+    'format_vers': 0.1,
+    'org': None,
+    'name': None,
+    'slug': None,
+    'number': None,
+    'node': {
+        'name': None,
+        'flavor': None,
+    },
+    'environment': {
+        'CI_SYSTEM': 'vpci',
+    },
+    'jobs': [
+        'noop',
+    ],
+}
+
+
+def process_haproxy(org, name, number):
+    job = job_format
+    job['org'] = org
+    job['name'] = name
+    job['number'] = number
+    job['slug'] = org + '/' + name
+    pp.pprint(job)
+    r.rpush('vpci_job_queue', json.dumps(job))
+
+def process_puppet_core(org, name, number):
+    return
+
 
 def pump(pr):
     org, name, number = pr.split('/')
@@ -11,24 +45,25 @@ def pump(pr):
     slug = org + '/' + name
     #TODO betterify this
     if slug == 'puppetlabs/puppetlabs-haproxy':
-        process_haproxy()
+        process_haproxy(org, name, number)
     if slug == 'puppetlabs/puppet':
-        process_puppet_core()
+        process_puppet_core(org, name, number)
 
 
 def main_loop():
     while True:
-        new_pr = r.lindex('new_pull_requests', 0)
-        if new_pr is None:
+        pr = r.lpop('new_pull_requests')
+        if pr is None:
             print "looping"
             time.sleep(5)
         else:
-            pump(new_pr)
+            pump(pr)
 
 
 
 
 if __name__ == "__main__":
+    pp = pprint.PrettyPrinter(indent=4)
 
     with open('config.yaml') as f:
         conf = yaml.load(f.read())
